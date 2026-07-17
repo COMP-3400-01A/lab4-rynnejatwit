954313b9e231d903700060f6bbb35b2fd42ccbda
rynnejatwit <rynnej@wit.edu>
2026-07-17 16:05:36 -0400

    Completed Part A

--- parta.c
+++ parta.c
@@ -1,8 +1,20 @@
 #include <stdbool.h>
 #include <stddef.h>
-
-// TODO: Include more header files (read the manpages to find out more!)
+#include <stdio.h>
+#include <ctype.h>
 
 int main(int argc, const char* argv[]) {
+    if (argc < 2) {
+        puts("ERROR: No arguments");
+        return 1;
+    }
+
+    for (int i = 1; i < argc; i++) {
+        if (i > 1) putchar(',');
+        for (const char* p = argv[i]; *p; p++)
+            putchar(toupper((unsigned char)*p));
+    }
+
+    putchar('\n');
     return 0;
 }
